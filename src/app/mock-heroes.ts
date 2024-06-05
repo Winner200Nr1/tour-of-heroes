bcce9b0ebcb87ffe8e47232e174058cb63b38a0d
studentNr-1 <102148944+studentNr-1@users.noreply.github.com>
2024-06-05 16:14:43 +0200

    Alles bis und mit Nr.4 Observable/ Subscribe

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './Hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', superkraft:"Being nice"},
+  { id: 13, name: 'Bombasto'},
+  { id: 14, name: 'Celeritas'},
+  { id: 15, name: 'Magneta' , },
+  { id: 16, name: 'RubberMan'},
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ', superkraft: "brain"},
+  { id: 19, name: 'Magma' , superkraft: "lava"},
+  { id: 20, name: 'Tornado' , superkraft: "Tornado"}
+];
